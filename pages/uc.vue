f55bddbb965b68b49bd6b9842f236483c6230485
lizhigao <lizhigao@021.com>
2020-07-23 18:16:37 +0800

    拖拽上传 & 上传进度条

--- pages/uc.vue
+++ pages/uc.vue
@@ -1,24 +1,69 @@
 <template>
-  <div>
-    <h1>用户中心</h1>
-    <input type="file" @change="handleUpload" />
+  <div class="uc-container">
+    <h2>用户中心</h2>
+    <el-form label-width="100px" class="uc-form">
+      <el-form-item label="昵称">
+        <span>{{ userInfo.nickname }}</span>
+      </el-form-item>
+      <el-form-item label="邮箱">
+        <span>{{ userInfo.email }}</span>
+      </el-form-item>
+    </el-form>
+    <hr />
+    <div ref="drag" id="drag">
+      <input type="file" @change="handleUpload" />
+    </div>
+    <div class="progress">
+      <el-progress :percentage="progress"></el-progress>
+    </div>
     <el-button type="primary" @click="upload">上传</el-button>
   </div>
 </template>
 
 <script>
 export default {
-  layout: 'blank',
   data() {
     return {
-      file: null
+      file: null,
+      progress: 0,
+      userInfo: {}
     };
   },
   mounted() {
     // 获取用户信息;
     this.$http.get('/user/info').then(res => {
       console.log('[user info]', res);
+      this.userInfo = res.data;
     });
+    const drag = this.$refs.drag;
+    // drag.addEventListener('dragenter', e => {
+    //   drag.style.borderColor = 'red';
+    //   e.preventDefault();
+    // });
+    drag.addEventListener('dragover', e => {
+      e.preventDefault();
+      drag.style.borderColor = 'green';
+      drag.style.backgroundColor = 'rgba(0, 255, 0, 0.2)';
+    });
+    drag.addEventListener('dragleave', e => {
+      e.preventDefault();
+      drag.style.borderColor = '#e5e5e5';
+      drag.style.backgroundColor = '';
+    });
+    drag.addEventListener('drop', e => {
+      e.preventDefault();
+      drag.style.borderColor = '#e5e5e5';
+      drag.style.backgroundColor = '';
+      console.log('e.dataTransfer', e.dataTransfer);
+      const fileList = e.dataTransfer.files;
+      this.file = fileList[0];
+    });
+    // let timer = setInterval(() => {
+    //   if (this.progress > 100) {
+    //     clearInterval(timer);
+    //   }
+    //   this.progress++;
+    // }, 500);
   },
   methods: {
     handleUpload(e) {
@@ -33,10 +78,29 @@ export default {
       // {
       //   headers: { 'content-type': 'multipart/form-data' }
       // }
-      this.$http.post('/uploadFile', fd);
+      this.$http.post('/uploadFile', fd, {
+        onUploadProgress: e => {
+          this.progress = Number(((e.loaded / e.total) * 100).toFixed(0));
+          // console.log(e, this.progress);
+        }
+      });
     }
   }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.uc-container {
+  max-width: 1000px;
+  margin: 0 auto;
+  padding: 30px;
+}
+#drag {
+  border: 2px dashed #e5e5e5;
+  margin: 15px 0;
+  padding: 30px;
+}
+.progress {
+  margin: 10px 0;
+}
+</style>
